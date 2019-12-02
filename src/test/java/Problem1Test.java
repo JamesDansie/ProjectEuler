56f04142e2821b21fc4bbf511d1aa030f0767275
JamesDansie <jdansie8336@gmail.com>
2019-12-02 15:35:17 -0800

    problem 1 done with tests

---
+++ src/test/java/Problem1Test.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Problem1Test {
+
+    @Test
+    public void threeAndFiveSum() {
+        assertEquals("The given problem of 10 should equal 23",
+                23,
+                Problem1.ThreeAndFiveSum(10));
+    }
+
+    @Test
+    public void smallNum(){
+        assertEquals("For 5 there should be 3+5 = 8",
+                8,
+                Problem1.ThreeAndFiveSum(6));
+    }
+
+    @Test
+    public void bigNum(){
+        assertEquals("This is a really big number",
+                233168,
+                Problem1.ThreeAndFiveSum(1000));
+    }
+}
