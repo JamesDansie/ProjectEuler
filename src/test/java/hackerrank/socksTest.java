aea857e016d3520ff1e7d7235bf4005c4311bf4f
JamesDansie <jdansie8336@gmail.com>
2020-04-20 17:31:17 -0700

    missing tests

--- src/test/java/hackerrank/socksTest.java
+++ src/test/java/hackerrank/socksTest.java
@@ -1,5 +1,16 @@
+package hackerrank;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class socksTest {
 
+    @Test
+    public void sockMerchant() {
+        int num_socks = 9;
+        int[] socks = new int[]{10, 20, 20, 10, 10, 30, 50, 10, 20};
+
+        System.out.println(Socks.sockMerchant(num_socks, socks));
+    }
 }
---
+++ src/test/java/leet/libertyTest.java
@@ -0,0 +1,14 @@
+package leet;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class libertyTest {
+
+    @Test
+    public void say() {
+        liberty lib = new liberty();
+        lib.say();
+    }
+}
